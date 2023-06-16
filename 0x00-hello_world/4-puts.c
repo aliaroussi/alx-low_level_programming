54838a052ef38bdb0bd41b6f35ddc812e158c4b4
aliaroussi <aliaroussi.mail@gmail.com>
2023-06-15 18:28:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	retur#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Hello, C");
+    return (0);
+}
+(0);
 }
